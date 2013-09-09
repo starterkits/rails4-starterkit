7b14b33309bbba555b6aba4bcc43e948ac0b80e8
Joe Johnston <joe@simple10.com>
2013-09-09 09:44:22 -0700

    Init
    rails new airship --database=postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airship::Application.config.secret_key_base = 'c4ae9ffbd76300d35947c3cd6dbab75947df833dd54511dfb0b83b5ce63335cbbeab6255d3b3775867f8d0e3b37728a14b18233c1bcb7ce2c6d8678666cc3cf0'
