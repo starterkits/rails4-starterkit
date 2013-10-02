4dd20cfe1dbc33ae4308ca83eca56e3ebffeb957
Joe Johnston <joe@simple10.com>
2013-10-02 12:51:19 -0700

    Turn on authorization checks by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Devise, require authenticate by default
+  before_filter :authenticate_user!
+
+  # CanCan, check authorization unless authorizing with devise
+  check_authorization unless: :devise_controller?
+
   include CommonHelper
   include ErrorReporting
 end
