6d61c0c2c9574df71e85ecaa9940c921a77cf2d6
Joe Johnston <joe@simple10.com>
2013-10-02 12:56:29 -0700

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ ExampleApp::Application.routes.draw do
   match '/error' => 'pages#error', via: [:get, :post], as: 'error_page'
 
   # OAuth
-  post '/auth/:provider/callback' => 'oauth#create'
-  match '/auth/failure' => 'oauth#auth_failure', via: [:get, :post]
+  get '/auth/:provider/callback' => 'oauth#create'
+  get '/auth/failure' => 'oauth#auth_failure'
   get '/auth/:provider' => 'oauth#passthru', as: 'provider_auth'
   get '/auth' => redirect('/a/signup')
 
   # User registration and login
   devise_for :users, path: '/a',
-    controllers: {registrations: 'users/registrations', passwords: 'users/reset_password'},
-    path_names: {sign_up: 'signup', sign_in: 'login', sign_out: 'logout'}
+      controllers: {registrations: 'users/registrations', passwords: 'users/reset_password'},
+      path_names: {sign_up: 'signup', sign_in: 'login', sign_out: 'logout'}
   get '/a' => redirect('/a/signup')
   get '/home' => 'users#show', as: 'user_home'
   get '/login/done' => 'users/auth#after_login', as: 'user_root'
