297ae84e904c602b0b4f7ff36af2ceb37f3a2512
Joe Johnston <joe@simple10.com>
2013-10-03 11:50:56 -0700

    Force test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ unless File.exists? '.zeus.sock'
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 # require 'rspec/autorun' # disabled for zeus
