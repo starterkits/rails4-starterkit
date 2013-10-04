4290229d2e9b0570bd673f6fdb72e15187d1303b
Joe Johnston <joe@simple10.com>
2013-10-03 23:15:56 -0700

    Rename auth config file for uniformity

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -10,6 +10,7 @@ end
 # TODO: confirm that full_host is no longer needed in dev
 # full_host = '' # set to url as needed
 # OmniAuth.config.full_host = full_host if full_host.present?
+OmniAuth.config.logger = Rails.logger
 
 ExampleApp::Application.config.middleware.use OmniAuth::Builder do
   ExampleApp::AuthConfig.providers.each do |k, v|
