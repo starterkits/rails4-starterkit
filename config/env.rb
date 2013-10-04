64f465b3c1c7151b4576d4db181f609419516f30
Joe Johnston <joe@simple10.com>
2013-10-03 23:15:27 -0700

    Add default env settings for demo

---
+++ config/env.rb
@@ -0,0 +1,5 @@
+# TODO: replace values below with development app credentials
+
+# 10 Hacks demo app credentials
+ENV['AUTH_TWITTER_KEY'] ||= 'hbcu8JvAB1FftWw7c3oog'
+ENV['AUTH_TWITTER_SECRET'] ||= 'uTspycZ4T7QAeA9QLGBb1zoLrR6K7ChBPdYt8F7nZc'
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+env = File.expand_path('../env.rb', __FILE__)
+load(env) if File.exists?(env)
+
 # Initialize the Rails application.
 ExampleApp::Application.initialize!
