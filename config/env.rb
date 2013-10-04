646198583d71e19ac65e5751f34c3ceaee1f7310
Joe Johnston <joe@simple10.com>
2013-10-04 09:21:48 -0700

    Add Facebook demo app credentials

--- config/env.rb
+++ config/env.rb
@@ -1,5 +1,7 @@
-# TODO: replace values below with development app credentials
+# Replace values below with development app credentials
 
 # 10 Hacks demo app credentials
+ENV['AUTH_FACEBOOK_KEY'] ||= '201539556694091'
+ENV['AUTH_FACEBOOK_SECRET'] ||= '80ae1dc93961b7f9a79265af0eeca698'
 ENV['AUTH_TWITTER_KEY'] ||= 'hbcu8JvAB1FftWw7c3oog'
 ENV['AUTH_TWITTER_SECRET'] ||= 'uTspycZ4T7QAeA9QLGBb1zoLrR6K7ChBPdYt8F7nZc'
