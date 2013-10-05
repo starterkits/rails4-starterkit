c773d42003aa78b3b32956b48870fd8a215f464a
Joe Johnston <joe@simple10.com>
2013-10-05 15:25:37 -0700

    Add expiration to sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-ExampleApp::Application.config.session_store :cache_store, key: '_exampleapp_session'
+ExampleApp::Application.config.session_store :cache_store,
+  { key: '_exampleapp_session', expire_after: 30.minutes }
+
