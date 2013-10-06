58c11e4cbe779e36e84a1c8b04f03bfa0bca62f3
Joe Johnston <joe@simple10.com>
2013-10-06 13:50:11 -0700

    Add ability for user to manage self

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user = User.new)
+    can :manage, User, id: user.id
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
