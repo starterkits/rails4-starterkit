4b0c06d5931453364ac2c273db532866c988a8ab
Joe Johnston <joe@simple10.com>
2013-10-09 13:20:23 -0700

    Use separate airbrake api key for javascript

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,9 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
-  config.host    = ENV['AIRBRAKE_HOST']
-  config.port    = 443
-  config.secure  = (config.port == 443)
+if defined? Airbrake
+  Airbrake.configure do |config|
+    config.api_key = ENV['AIRBRAKE_API_KEY']
+    config.host    = ENV['AIRBRAKE_HOST']
+    config.port    = 443
+    config.secure  = (config.port == 443)
+    config.js_api_key = ENV['AIRBRAKE_JS_API_KEY']
+  end
 end
