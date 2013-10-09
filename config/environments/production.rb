a1a0d6cc25356895f803c9f5da64940535a40cb8
Joe Johnston <joe@simple10.com>
2013-10-09 15:23:01 -0700

    Use tagged logger in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ StarterKit::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Use a different cache store in production.
   # If using Memcachier on Heroku, the memcachier gem will auto config servers
