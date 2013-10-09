9fed3745253dfabcc9127d582e8eba1d90bb076c
Joe Johnston <joe@simple10.com>
2013-10-09 11:11:47 -0700

    Add tests to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe User do
   let(:user) { FactoryGirl.build(:user) }
-  let(:authentication) { FactoryGirl.build(:authentication) }
+  let(:auth) { FactoryGirl.build(:authentication) }
 
   describe "#create" do
-    it "should require password" do
+    it "should require password or authentication" do
       user.password = nil
       user.should_not be_valid
       user.password = 'testpass'
+      user.should be_valid
+      user.password = nil
+      user.authentications << auth
       user.should be_valid
     end
     it "should require email" do
@@ -19,14 +22,25 @@ describe User do
     end
   end
 
-  describe "#destroy" do
-    it "should remove authentications" do
-      user.save
-      authentication.user = user
-      authentication.save
-      Authentication.where(user_id: user.id).count.should == 1
-      user.destroy
-      Authentication.where(user_id: user.id).count.should == 0
+  describe "#authentications" do
+    it "should have many dependent authentications" do
+      user.should have_many(:authentications).dependent(:destroy)
+    end
+  end
+
+  describe "#reverse_merge_attributes_from_auth" do
+    it "should merge email" do
+      user.email = nil
+      email = 'auth@domain.com'
+      auth.oauth_data = {email: email}
+      user.reverse_merge_attributes_from_auth(auth)
+      user.email.should == email
+    end
+    it "should not merge email" do
+      email = 'somerandom@email.com'
+      auth.oauth_data = {email: email}
+      user.reverse_merge_attributes_from_auth(auth)
+      user.email.should_not == email
     end
   end
 end
