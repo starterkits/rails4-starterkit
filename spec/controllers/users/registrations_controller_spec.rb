6bfb06ebdbc397edd7304281827ee5501a246bbb
Joe Johnston <joe@simple10.com>
2013-10-09 11:17:21 -0700

    Fix registration controller spec; all green

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -5,7 +5,8 @@ describe Users::RegistrationsController do
     it "should redirect to after sign up url" do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       post :create, user: {first_name: 'Test', email: 'test@example.com', password: 'testpass'}
-      URI(response.redirect_url).path.should == after_sign_up_path(controller.resource.id)
+      path = controller.send(:after_sign_up_path_for, controller.resource)
+      URI(response.redirect_url).path.should == path
     end
   end
 end
