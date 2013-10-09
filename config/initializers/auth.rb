f9f019910e1e67e4e526f71df1d166856cf14760
Joe Johnston <joe@simple10.com>
2013-10-09 15:53:09 -0700

    Revert "Try hardcoding providers"
    This reverts commit f8d3a7827e00fd527e713f7e82e33085248fc80c.

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -10,10 +10,8 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.path_prefix = '/o'
 
 StarterKit::Application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'suaKRakoutQv18PgRlGaw', 'lDOG2C6pkiSxV8RRon8leOj5ZLJeNoqLJziwfXhgtw'
-  provider :facebook, '1391555207746028', '518f4a8ae5229e8ed432f1cf6806b480'
-  # StarterKit::AuthConfig.providers.each do |k, v|
-  #   opts = (v.try(:[], 'oauth') || {}).symbolize_keys
-  #   provider k, v['key'], v['secret'], opts
-  # end
+  StarterKit::AuthConfig.providers.each do |k, v|
+    opts = (v.try(:[], 'oauth') || {}).symbolize_keys
+    provider k, v['key'], v['secret'], opts
+  end
 end
