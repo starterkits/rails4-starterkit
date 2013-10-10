5e51d4b71110aeb06ec543476caa6da62e1dfc51
Joe Johnston <joe@simple10.com>
2013-10-09 18:40:06 -0700

    Restore auth initializer

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -11,16 +11,10 @@ OmniAuth.config.path_prefix = '/o'
 
 StarterKit::Application.config.middleware.use OmniAuth::Builder do
   StarterKit::AuthConfig.providers.each do |k, v|
-    # opts = (v.try(:[], 'oauth') || {}).symbolize_keys
-    #opts.merge!({client_options: {ssl: {ca_file: Rails.root.join('lib/assets/certs/cacert.pem').to_s}}})
-    # opts.merge!({client_options: { ssl: {
-    #   ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-    #   ca_path: "/etc/ssl/certs"
-    # }}})
-    # provider k, v['key'], v['secret'], opts
-    provider :facebook, '1391555207746028', '518f4a8ae5229e8ed432f1cf6806b480', {client_options: { ssl: {
-      ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-      ca_path: "/etc/ssl/certs"
-    }}}
+    opts = (v.try(:[], 'oauth') || {}).symbolize_keys
+    if Rails.env.development?
+      opts.merge!({client_options: {ssl: {ca_file: Rails.root.join('lib/assets/certs/cacert.pem').to_s}}})
+    end
+    provider k, v['key'], v['secret'], opts
   end
 end
