3a575771c8daba7ba9efcc0aaa7635d9dfd3e4d0
Joe Johnston <joe@simple10.com>
2013-10-09 19:06:13 -0700

    Set action_mailer url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ StarterKit::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
