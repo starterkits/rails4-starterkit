f02cec4093b3873763170ab1c3eb8655c2cde73e
Joe Johnston <joe@simple10.com>
2013-10-09 19:17:36 -0700

    Debug heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,9 @@ StarterKit::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  puts '!!!!!'
+  puts ENV['HOST']
+  config.action_mailer.default_url_options = { host: ENV['HOST'] || 'host.nope' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
