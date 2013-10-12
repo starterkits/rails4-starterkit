eded39e02da12a6cef5eafe3eed4a2aeccd8e4fd
Joe Johnston <joe@simple10.com>
2013-10-12 09:59:34 -0700

    Add tests for DeviseReturnToConcern

--- app/controllers/concerns/devise_return_to_concern.rb
+++ app/controllers/concerns/devise_return_to_concern.rb
@@ -1,6 +1,9 @@
 module DeviseReturnToConcern
   extend ActiveSupport::Concern
 
+  # Amount of time in seconds allowed before cached return to path is ignored
+  RETURN_TO_TIMEOUT = 600
+
   included do
     before_action :store_location!, only: :new
   end
@@ -16,8 +19,8 @@ module DeviseReturnToConcern
     url = stored_location_for(resource)
 
     # Ignore any request that originated over 10 minutes ago
-    time = Time.now.utc.to_i - 600
-    req_time = session.delete(":#{resource_name}_return_to_timestamp")
+    time = Time.now.utc.to_i - RETURN_TO_TIMEOUT
+    req_time = session.delete(:"#{resource_name}_return_to_timestamp")
     url = nil if req_time && req_time < time
 
     url || user_root_path
---
+++ spec/controllers/concerns/devise_return_to_concern_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+class DummyController < DeviseController
+  include DeviseReturnToConcern
+  attr_accessor :params, :session, :resource_name
+  def initialize
+    @params = {}.with_indifferent_access
+    @session = {}.with_indifferent_access
+    @resource_name = 'user'
+  end
+  def is_navigational_format?; true; end
+end
+
+describe DeviseReturnToConcern do
+  let(:controller) { DummyController.new }
+
+  describe "store_location!" do
+    it "should set session when return_to param is present" do
+      path = '/some/test/path'
+      controller.params = { return_to: path }
+      controller.store_location!
+      controller.session[:"#{controller.resource_name}_return_to"].should == path
+      controller.session[:"#{controller.resource_name}_return_to_timestamp"].should >= Time.now.utc.to_i
+      controller.stored_location_for(controller.resource_name).should == path
+    end
+    it "should not change session when return_to is missing" do
+      controller.store_location!
+      controller.session[:"#{controller.resource_name}_return_to"].should be_nil
+      controller.session[:"#{controller.resource_name}_return_to_timestamp"].should be_nil
+      controller.stored_location_for(controller.resource_name).should be_nil
+    end
+  end
+
+  describe "after_sign_in_path_for" do
+    it "should return stored location" do
+      path = '/some/test/path'
+      controller.params = { return_to: path }
+      controller.store_location!
+      controller.after_sign_in_path_for(controller.resource_name).should == path
+    end
+    it "should not return stored path if expired" do
+      path = '/some/test/path'
+      user_root_path = '/user/root'
+      controller.stub(user_root_path: user_root_path)
+      controller.params = { return_to: path }
+      controller.store_location!
+      controller.session[:"#{controller.resource_name}_return_to_timestamp"] = \
+        Time.now.utc.to_i - DummyController::RETURN_TO_TIMEOUT - 10
+      controller.after_sign_in_path_for(controller.resource_name).should_not == path
+      controller.after_sign_in_path_for(controller.resource_name).should == user_root_path
+    end
+  end
+end
