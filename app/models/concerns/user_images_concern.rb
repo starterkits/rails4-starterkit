6981ea0fa4f103320842d1e4477e4f56ef93c7a7
Joe Johnston <joe@simple10.com>
2013-10-20 17:42:05 -0700

    Add options to User#image_url

--- app/models/concerns/user_images_concern.rb
+++ app/models/concerns/user_images_concern.rb
@@ -1,17 +1,96 @@
 module Concerns::UserImagesConcern
   extend ActiveSupport::Concern
 
-  def gravatar_url
-    if email.blank?
-      nil
-    else
-      "https://secure.gravatar.com/avatar/#{md5_email}"
-    end
+  # Default image sizes are the same as Twitter's.
+  # https://dev.twitter.com/docs/user-profile-images-and-banners
+  # Modify provider specific image methods as needed after changing IMAGE_SIZES.
+  IMAGE_SIZES = {
+    tiny:   24,
+    thumb:  48,
+    large:  73
+  }.freeze
+
+  # ssl: true|false
+  # size: :tiny|:thumb|:large
+  def image_url(*opts)
+    sized_image_url(self[:image_url], *opts) || gravatar_url(*opts)
+  end
+
+  def sized_image_url(url, size: :thumb, ssl: true)
+    return nil unless url.present?
+    type = image_url_type(url)
+    return url if type == :url
+    self.send("sized_#{type}_image_url", url, size: size, ssl: ssl)
+  end
+
+  def gravatar_url(ssl: true, size: :thumb)
+    return nil unless email.present?
+    url = ssl ? 'https://secure.' : 'http://'
+    "#{url}gravatar.com/avatar/#{md5_email}?s=#{image_size(size)}"
+  end
+
+  def image_size(size)
+    IMAGE_SIZES[size].presence || IMAGE_SIZES[:thumb]
+  end
+
+  def image_url_ssl(url)
+    url.gsub(/^http:\/\//i, '^https://')
   end
 
   def md5_email
     require 'digest/md5'
     @md5_email = nil if email_changed?
     @md5_email ||= Digest::MD5.hexdigest(email.strip.downcase)
+  end
+
+  def image_url_type(url)
+    return nil unless url.present?
+    case url
+    when /twitter_production|twimg\.com/
+      :twitter
+    when /graph\.facebook/
+      :facebook
+    when /licdn|linkedin/
+      :linkedin
+    else
+      :url
+    end
+  end
+
+  def sized_twitter_image_url(url, size: :thumb, ssl: true)
+    # https://dev.twitter.com/docs/user-profile-images-and-banners
+    size = case size
+    when :tiny then '_mini'     # 24x24
+    when :thumb then '_normal'  # 48x48
+    when :large then '_bigger'  # 73x73
+    else ''
+    end
+    url = image_url_ssl(url) if ssl
+    url.gsub(/(_(bigger|normal|mini))?\.(png|gif|jpeg|jpg)/, "#{size}.$3")
+  end
+
+  def sized_facebook_image_url(url, size: :thumb, ssl: true)
+    # https://developers.facebook.com/docs/reference/api/using-pictures/
+    url = image_url_ssl(url) if ssl
+
+    # Facebook's default sizes serve images significantly faster
+    # size = case size
+    # when :tiny then 'square'    # 50x50
+    # when :thumb then 'small'    # 50xHeight
+    # when :normal then 'normal'  # 100xHeight
+    # when :large then 'large'    # 200xHeight
+    # else 'normal'
+    # end
+    # url.gsub(/picture(\?type=[^&]*)?/, "picture?type=#{size}")
+
+    # Facebook supports on the fly image resizing
+    width = height = IMAGE_SIZES[size] || 100
+    url.gsub(/picture(\?type=[^&]*)?/, "picture?width=#{width}&height=#{height}")
+  end
+
+  def sized_linkedin_image_url(url, size: :thumb, ssl: true)
+    url = image_url_ssl(url.gsub(/[^\.\/]+\.(licdn|linkedin)\.com/, 'www.linkedin.com')) if ssl
+    width = height = IMAGE_SIZES[size] || 100
+    url.gsub(/\/shrink_\d+_\d+\//, "/shrink_#{width}_#{height}/")
   end
 end
--- spec/models/concerns/user_images_concern_spec.rb
+++ spec/models/concerns/user_images_concern_spec.rb
@@ -3,26 +3,46 @@ require 'spec_helper'
 describe Concerns::UserImagesConcern do
   let(:user) { FactoryGirl.build(:user) }
 
+  describe "#image_url" do
+    it "returns image_url if present" do
+      url = 'http://example.com/image.png'
+      user.image_url = url
+      user.image_url.should == url
+    end
+    it "returns gravatar when image_url is not set" do
+      user.image_url = nil
+      user.image_url.should =~ /gravatar\.com/
+    end
+    pending it "should check facebook, linkedin, twitter sizes"
+  end
+
   describe "#gravatar_url" do
     it "is nil if email is blank" do
       user.email = nil
       user.gravatar_url.should be_blank
     end
-
     it "returns correct url" do
       require 'digest/md5'
       email = 'Test@Example.COM'
       user.email = email
       md5_email = Digest::MD5.hexdigest(email.strip.downcase)
-      user.gravatar_url.should ==  "https://secure.gravatar.com/avatar/#{md5_email}"
+      user.gravatar_url(size: :thumb, ssl: true).should ==  "https://secure.gravatar.com/avatar/#{md5_email}?s=#{User::IMAGE_SIZES[:thumb]}"
+      user.gravatar_url(size: :thumb, ssl: false).should ==  "http://gravatar.com/avatar/#{md5_email}?s=#{User::IMAGE_SIZES[:thumb]}"
     end
-
     it "updates when email changes" do
       email = 'text@example.com'
       old_gravatar = user.gravatar_url
       user.email.should_not == email
       user.email = email
       user.gravatar_url.should_not == old_gravatar
+    end
+    it "returns correct size" do
+      User::IMAGE_SIZES.each do |size, value|
+        user.gravatar_url(size: size).should =~ /s=#{value}$/
+      end
+    end
+    it "returns thumb if size is invalid" do
+      user.gravatar_url(size: :invalid_size).should =~ /s=#{User::IMAGE_SIZES[:thumb]}$/
     end
   end
 end
