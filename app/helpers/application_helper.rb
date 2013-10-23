700e3ada2ea9ca73d5d91db7730d04ec44b97986
Joe Johnston <joe@simple10.com>
2013-10-23 15:59:10 -0700

    Add render_once view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
   include CommonHelper
+
+  # Render a partial only one time.
+  #
+  # Useful for rendering partials that require JavaScript like Google Maps
+  # where other views may have also included the partial.
+  def render_once(view, *args, &block)
+    @_render_once ||= {}
+    if @_render_once[view]
+      nil
+    else
+      @_render_once[view] = true
+      render(view, *args, &block)
+    end
+  end
 end
