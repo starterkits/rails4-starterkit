b7854dcd59b193d33f3196cbbcf5201c52bb4980
Joe Johnston <joe@simple10.com>
2013-11-17 20:28:04 -0800

    Add common helper methods

--- app/helpers/common_helper.rb
+++ app/helpers/common_helper.rb
@@ -5,4 +5,31 @@ module CommonHelper
   include GuidHelper
   include OauthHelper
 
+  # Check if resource has a specific error.
+  #
+  # Requires I18n::Backend::Metadata is enabled.
+  # Example:
+  #   resource_has_error? resource, :email, :blank
+  #   # Returns true if resource failed validates_presence_of for email field.
+  def resource_has_error?(resource, field, error_key)
+    error_key = error_key.to_s
+    return false unless errors = resource.errors.get(field)
+    errors.each do |msg|
+      key = msg.try(:translation_metadata).try(:[], :key)
+      return true if key && key.to_s.index(error_key)
+    end
+    false
+  end
+
+  # Add query parameters to a URL.
+  #
+  # Example:
+  #   add_to_url('/a/b?test=1', data: 'yes')
+  #   => '/a/b?test=1&data=yes'
+  def add_to_url(url, params)
+    uri = URI.parse(url)
+    query = URI.escape params.collect{|k,v| "#{k}=#{v}" }.join('&')
+    uri.query = [uri.query, query].compact.join('&')
+    uri.to_s
+  end
 end
