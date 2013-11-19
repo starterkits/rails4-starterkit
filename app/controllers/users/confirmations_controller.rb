3a4677a6ce69905bbae690a2182b46b28da5f5a3
Joe Johnston <joe@simple10.com>
2013-11-19 00:13:02 -0800

    Improve ux confirm email process when signed in

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,2 +1,20 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  def new
+    self.resource = (current_user or resource_class.new)
+  end
+
+  def create
+    if user_signed_in?
+      Devise.paranoid = false
+      params[resource_name] ||= {}
+      params[resource_name][:email] = current_user.email
+    end
+    super
+  end
+
+  protected
+
+  def after_resending_confirmation_instructions_path_for(resource_name)
+    user_signed_in? and edit_user_registration_path or super
+  end
 end
