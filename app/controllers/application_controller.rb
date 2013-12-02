87181155257ffe4255e16f769793f2736c0f0fbe
Joe Johnston <joe@simple10.com>
2013-12-02 02:01:17 -0800

    Add basic auth support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  if ENV['BASIC_AUTH']
+    user, pass = ENV['BASIC_AUTH'].split(':')
+    http_basic_authenticate_with name: user, password: pass
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
