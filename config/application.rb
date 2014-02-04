bbbef344c2bfdf98cac6efedfbfe10d88ff346ab
Joe Johnston <joe@simple10.com>
2014-02-04 09:50:06 -0800

    Update requires in application.rb to rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
