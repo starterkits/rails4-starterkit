54df11c5b0e56ea202d761d4600ec9a5a4755dc3
Joe Johnston <joe@simple10.com>
2014-02-10 15:15:38 -0800

    Allow debugging of sidekiq

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,7 @@ if ENV['RAILS_ENV'] == 'development' or not ENV['RAILS_ENV']
   # development
   worker_processes 1
   listen 3000
+  preload_app true
 
 else
 
@@ -13,28 +14,29 @@ else
   timeout 30
   preload_app true
 
-  before_fork do |server, worker|
-    Signal.trap 'TERM' do
-      puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-      Process.kill 'QUIT', Process.pid
-    end
+end
 
-    # Comment out if running workers in separate processes via Procfile
-    if defined? Sidekiq
-      @sidekiq_pid ||= spawn("bundle exec sidekiq -C config/sidekiq.yml")
-    end
 
-    defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
 
-  after_fork do |server, worker|
-    Signal.trap 'TERM' do
-      puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-    end
-
-    defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
+  # Comment out if running workers in separate processes via Procfile
+  if defined? Sidekiq
+    @sidekiq_pid ||= spawn("bundle exec sidekiq -C config/sidekiq.yml")
   end
 
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
