f20a6f204d76c0d9b6b67a284caf1f90b427f7a1
Joe Johnston <joe@simple10.com>
2014-02-10 18:24:20 -0800

    Disable Rspec::Sidekiq warning message in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,13 +71,14 @@ RSpec.configure do |config|
 end
 
 # https://github.com/philostler/rspec-sidekiq
-# RSpec::Sidekiq.configure do |config|
-#   # Clears all job queues before each example
-#   config.clear_all_enqueued_jobs = true # default => true
+RSpec::Sidekiq.configure do |config|
+  # Clears all job queues before each example
+  config.clear_all_enqueued_jobs = true # default => true
 
-#   # Whether to use terminal colours when outputting messages
-#   config.enable_terminal_colours = true # default => true
+  # Whether to use terminal colours when outputting messages
+  config.enable_terminal_colours = true # default => true
 
-#   # Warn when jobs are not enqueued to Redis but to a job array
-#   config.warn_when_jobs_not_processed_by_sidekiq = true # default => true
-# end
+  # Warn when jobs are not enqueued to Redis but to a job array
+  config.warn_when_jobs_not_processed_by_sidekiq = false # default => true
+end
+
