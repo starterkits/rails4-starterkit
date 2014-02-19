c58eceec6019edac335a8c1155db29f0085d32d5
Joe Johnston <joe@simple10.com>
2014-02-19 10:24:11 -0800

    Add new rails 4.1.0.rc1 configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,12 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
