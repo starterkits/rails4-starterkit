afa80e3fd2f330bee5d8f87a408a90a47884b329
Joe Johnston <joe@simple10.com>
2014-02-20 13:10:36 -0800

    Scroll to alert box on login error

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,10 @@ class Users::SessionsController < Devise::SessionsController
   def new
     @failed = params[:failed]
     @provider = params[:provider]
+    if request.env.try(:[], 'warden.options').try(:[], :action) == 'unauthenticated'
+      @failed_login = flash.alert
+      flash.delete :alert
+    end
     return render 'failed' if @failed
     super
   end
