b0556d45e8e100a63588893f1f20392df58b9b04
Joe Johnston <joe@simple10.com>
2014-10-19 22:37:04 -0700

    Update schema after gem upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -16,43 +16,6 @@ ActiveRecord::Schema.define(version: 20140204233952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authentications", force: true do |t|
-    t.integer  "user_id"
-    t.string   "provider",      null: false
-    t.string   "proid",         null: false
-    t.string   "token"
-    t.string   "refresh_token"
-    t.string   "secret"
-    t.datetime "expires_at"
-    t.string   "username"
-    t.string   "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["provider"], :name => "index_authentications_on_provider"
-    t.index ["user_id"], :name => "fk__authentications_user_id"
-  end
-
-  create_table "oauth_caches", id: false, force: true do |t|
-    t.integer  "authentication_id", null: false
-    t.text     "data_json",         null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["authentication_id"], :name => "fk__oauth_caches_authentication_id"
-    t.index ["authentication_id"], :name => "index_oauth_caches_on_authentication_id"
-  end
-
-  create_table "rails_admin_histories", force: true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      limit: 2
-    t.integer  "year",       limit: 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
-  end
-
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -82,6 +45,45 @@ ActiveRecord::Schema.define(version: 20140204233952) do
     t.index ["is_admin"], :name => "index_users_on_is_admin"
     t.index ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
     t.index ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+  end
+
+  create_table "authentications", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider",      null: false
+    t.string   "proid",         null: false
+    t.string   "token"
+    t.string   "refresh_token"
+    t.string   "secret"
+    t.datetime "expires_at"
+    t.string   "username"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["provider"], :name => "index_authentications_on_provider"
+    t.index ["user_id"], :name => "fk__authentications_user_id"
+    t.foreign_key ["user_id"], "users", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "fk_authentications_user_id"
+  end
+
+  create_table "oauth_caches", id: false, force: true do |t|
+    t.integer  "authentication_id", null: false
+    t.text     "data_json",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["authentication_id"], :name => "fk__oauth_caches_authentication_id"
+    t.index ["authentication_id"], :name => "index_oauth_caches_on_authentication_id"
+    t.foreign_key ["authentication_id"], "authentications", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "fk_oauth_caches_authentication_id"
+  end
+
+  create_table "rails_admin_histories", force: true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
   end
 
 end
