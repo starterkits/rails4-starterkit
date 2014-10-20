4617c8045e4052467064cd3238430e644c992cd4
Joe Johnston <joe@simple10.com>
2014-10-20 08:19:20 -0700

    Enable common rspec options

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ unless File.exist? '.zeus.sock'
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] = 'test'
+ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
