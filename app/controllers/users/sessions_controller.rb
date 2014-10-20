a0865d9db7fe0bdd510737100762e278e53ca8ad
Joe Johnston <joe@simple10.com>
2014-10-20 14:19:08 -0700

    Fix issue with session#destroy not working
    If user is logged out, the session does not 
    get properly destroyed by Devise.
    
    https://github.com/starterkits/rails4-starterkit/issues/32

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
   include DeviseReturnToConcern
+  prepend_before_filter :force_reset_session, only: :destroy
 
   def new
     @failed = params[:failed]
     @provider = params[:provider]
     return render 'failed' if @failed
     super
+  end
+
+  def force_reset_session
+    reset_session
   end
 end
--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Users::SessionsController, :type => :controller do
+
   let(:user) { FactoryGirl.create(:user, password: 'testpass') }
 
   describe "#create" do
@@ -8,6 +9,32 @@ describe Users::SessionsController, :type => :controller do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       post :create, user: {email: user.email, password: 'testpass'}
       expect(response).to redirect_to(user_root_path)
+    end
+  end
+
+  describe "#destroy" do
+    include Warden::Test::Helpers
+    Warden.test_mode!
+
+    before :each do
+      Warden.test_reset!
+    end
+    it "resets session when user is logged in" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      login_as user, :scope => :user
+      sess_key = '_dummy_sess_key1'
+      sess_val = 'someval12345'
+      session[sess_key] = sess_val
+      post :destroy
+      expect(session[sess_key]).to_not eq(sess_val)
+    end
+    it "resets session when user is not logged in" do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      sess_key = '_dummy_sess_key2'
+      sess_val = 'someotherval9999'
+      session[sess_key] = sess_val
+      post :destroy
+      expect(session[sess_key]).to_not eq(sess_val)
     end
   end
 end
