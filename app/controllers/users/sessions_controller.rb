6ff2cdc8963a16b29037a99fc89885cf8c10e178
Joe Johnston <joe@simple10.com>
2014-10-20 14:19:08 -0700

    Fix issue with session#destroy not working
    If user is logged out, the session does not
    get properly destroyed by Devise.
    
    https://github.com/starterkits/rails4-starterkit/issues/32

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   include DeviseReturnToConcern
+  prepend_before_filter :force_reset_session, only: :destroy
 
   def new
     @failed = params[:failed]
@@ -10,5 +11,9 @@ class Users::SessionsController < Devise::SessionsController
     end
     return render 'failed' if @failed
     super
+  end
+
+  def force_reset_session
+    reset_session
   end
 end
